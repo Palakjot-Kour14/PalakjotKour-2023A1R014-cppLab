8e26fa6a6faa59b97d8df6241c4cd34f484d5287
Palakjot Kour <146604767+Palakjot-Kour14@users.noreply.github.com>
2025-03-03 14:49:38 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,18 @@
+/* Program to  show c working of onstructor
+*/
+# include <iostream>
+using namespace std;
+class Program //class name
+{
+    public:Program() //Default Constructor
+    {
+        cout <<" Default Constructor ";
+    }
+};
+int main()
+{
+    Program obj; //class object
+  return 0;
+}
+
+
